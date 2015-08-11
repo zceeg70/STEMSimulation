027390a238020e9c4fec0d293b634faad7b982af
peter <peter_morris461@hotmail.com>
2015-08-12 00:00:08 +0100

    vectors and drawing students

--- objects.py
+++ objects.py
@@ -21,12 +21,43 @@ def NewStudent():
     IDList.append([current_id,newStudent])
     return newStudent
 
-    
-class Student:
+class Vector:
+    def __init__(self):
+        self.x = -1
+        self.y = -1
+        self.dx = 0
+        self.dy = 0
+        #print("Vector inited")
+        
+    def SetPosition(self,x,y,dx=0,dy=0):
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        
+    def SetSpeed(self,dx,dy):
+        self.dx = dx
+        self.dy = dy
+
+    def GetPosition(self):
+       # print("GP Called")
+        return (self.x,self.y)
+
+    def GetSpeed(self):
+        return (self.dx,self.dy)
+
+    def UpdatePos(self,times=1):
+        self.x += self.dx*times
+        self.y += self.dy*times
+
+class Student(Vector):
     def __init__(self,interest,engagement,uniqueID="unassigned"):
+        super().__init__()
         self.interest = interest
         self.engagement = engagement
         self.uniqueID = uniqueID
+        self.type = "Student"
+        
     def setID(self,ID):
         self.uniqueID = ID
     
@@ -36,22 +67,24 @@ class Cell:
         pass
 
 class Classroom:
-    objectList = []
+    #objectList = []
     def __init__(self,pSize=(10,5),pType="classroom"):
         if(type(pSize)!=tuple):
             print("pSize must be tuple")
             sys.exit()
         self.roomSize = pSize
         self.roomType = pType
-        objectList = self.objectList
-        for x in range(0,pSize[0]):
-            row = []
-            for y in range(0,pSize[1]):
-                row.append({"Objects":[]})
-                #self.objectList[x][y] = {"Objects":[]}
-            objectList.append(row)
-        self.objectList = objectList
-                
+##        objectList = self.objectList
+##        for x in range(0,pSize[0]):
+##            row = []
+##            for y in range(0,pSize[1]):
+##                row.append({"Objects":[]})
+##                #self.objectList[x][y] = {"Objects":[]}
+##            objectList.append(row)
+        self.objectList = []
+    def GetSize(self):
+        return self.roomSize
+    
     def AddObject(self,pObject):
         #if not pObject in studentList: #<--TODO
         
@@ -83,12 +116,14 @@ class Classroom:
 
     def PrintObjects(self):
         objectList = self.objectList
-        for row_idx,row in enumerate(objectList):
-##        print("Row:{}".format(row))
-            for col_idx,col in enumerate(objectList[0]):
-                objectSing = objectList[row_idx][col_idx]["Objects"]
-                if(len(objectSing)>0):
-                    print("({},{}):{}".format(row_idx,col_idx,objectSing[0].uniqueID))
+        for o in objectList:
+            print("({}):{}".format(o.GetPosition(),o.type))
+##        for row_idx,row in enumerate(objectList):
+####        print("Row:{}".format(row))
+##            for col_idx,col in enumerate(objectList[0]):
+##                objectSing = objectList[row_idx][col_idx]["Objects"]
+##                if(len(objectSing)>0):
+##                    print("({},{}):{}".format(row_idx,col_idx,objectSing[0].uniqueID))
                     
     def IsPositionFree(self,position):
         x = position[0]
@@ -102,14 +137,16 @@ class Classroom:
         maxCount = self.roomSize[0]*self.roomSize[1]
         counter = 0
         objectList = self.objectList
-        
+##        positionList = [i.GetPosition()  for i in objectList]
+        positionList = lambda i:i.GetPosition(),objectList
         for counter in range(0,maxCount):
-            x = random.randint(0,(self.roomSize[0]-1))
-            y = random.randint(0,(self.roomSize[1]-1))
+            x = random.randint(2,(self.roomSize[0]-1))
+            y = random.randint(2,(self.roomSize[1]-1))
             #print("len(OL):{} Len(OL[0]):{} X:{} Y:{}".format(len(objectList),len(objectList[0]),x,y))
-            if(len(objectList[x][y]["Objects"])==0):
+            if((x,y) not in positionList):
                 print("Objected added at x:{},y:{}".format(x,y))
-                objectList[x][y]["Objects"].append(pObject)
+                pObject.SetPosition(x,y)
+                objectList.append(pObject)
                 self.objectList = objectList
                 #self.PrintObjects()
                 return True
--- simVer1.py
+++ simVer1.py
@@ -15,9 +15,29 @@ def PopulateClassroom(students,classroom):
         result = classroom.AddObject(student)
         if result == False:
             return False
+        
+def DrawCircles(room,mainscreen,scale=10):
+    objectList = room.GetObjectList()
+    offsetX = width/2
+    offsetY = height/2
+    
+    roomSize = room.GetSize()
+    roomWidth = roomSize[0]*scale
+    roomHeight = roomSize[1]*scale
+    roomTLX = offsetX-(roomWidth/2)
+    roomTLY = offsetY-(roomHeight/2)
+    
+    pygame.draw.rect(mainscreen,(255,0,0),(roomTLX,roomTLY,roomWidth,roomHeight),3)
+    for o in objectList:
+        pos = o.GetPosition()
+        posX =round(roomTLX+(scale*pos[0]))
+        posY = round(roomTLY+(scale*pos[1]))
+        radius = 5*scale
+        pygame.draw.circle(mainscreen,(255,0,0),(posX,posY),radius,1)
+        
 
 def main():
-    r1 = objects.Classroom(pSize = (20,6),pType="classroom")
+    r1 = objects.Classroom(pSize = (20,10),pType="classroom")
     PopulateClassroom(10,r1)
     test = r1.GetObjectList()
 ##    for row_idx,row in enumerate(test):
@@ -26,20 +46,35 @@ def main():
 ##            print("({},{}):{}".format(row_idx,col_idx,test[row_idx][col_idx]['Objects'].uniqueID))
     #print("classroom:{}".format(test))
     r1.PrintObjects()
-    screenEnabled = False
+    screenEnabled = True
     renderRate = 1.0/10.0
     print("Render rate:{}".format(renderRate))
     nextRenderUpdate = time.time() + renderRate
-    mainscreen.fill([0,0,255])    
+    
+    mainscreen.fill([0,0,255])
+    DrawCircles(r1,mainscreen)
+    currentZoom = 10
     while screenEnabled:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 screenEnabled = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    currentZoom += 5
+                    mainscreen.fill([0,0,255])
+                    DrawCircles(r1,mainscreen,currentZoom)
+                elif event.button == 5:
+                    currentZoom -= 5
+                    mainscreen.fill([0,0,255])
+                    DrawCircles(r1,mainscreen,currentZoom)
+                    
+               # print("Mouse:{}".format(pygame.mouse.get_pressed()))
+                
     
         if time.time() >= nextRenderUpdate:
             nextRenderUpdate += renderRate
             print("Updating screen: {}".format(nextRenderUpdate))
-            #pygame.display.flip()
+            pygame.display.flip()
         #pygame.time.delay(20)
     pygame.quit()
 
